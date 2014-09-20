c400df3c8f0bc2737cf73aff1d5cb78ade0a1b6f
Daniel Petkevich <daniel.petkevich@tcc-danielpetk.local>
2014-09-20 16:10:07 -0700

    Initial Commit

---
+++ Math&Animals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Math&Animals
+//
+//  Created by Daniel Petkevich on 9/20/14.
+//  Copyright (c) 2014 Daniel Petkevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
